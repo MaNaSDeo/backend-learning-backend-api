393946f84eba6ba93add3952eb7005ce26735cd3
MaNaSDeo <rmnslogin@gmail.com>
2024-03-11 23:19:59 +0530

    skip & limit, pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select("name price");
+  const products = await Product.find({}).limit(10).skip(3);
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -32,8 +32,14 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
-  res.status(200).json({ products, nbHits: products.length });
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
